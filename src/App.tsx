f44c3b7440b5bfc2af131070c6e6bffd57ada810
rendezvous14 <offkung14@gmail.com>
2022-07-29 19:28:54 +0700

    change text

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Trust me, I'm a QA. I'm your friend.
-        </p>
+        <p>"Trust me, I'm a QA. I'm your best friend."</p>
+        <p>I will help you.</p>
+        <p>I will help team.</p>
+        <p>I will find bugs.</p>
+        <p>I will not delay your release plan.</p>
+        <p>I give you my word I promise.</p>
+        <p>Nevertheless, I have to create a ticket for this shit. </p>
       </header>
     </div>
   );
